df7f25ba79459ff139b8a60beba6e80221d00d4b
kanhu.charan.sahoo.tpr <kanhu.charan.sahoo.tpr@vialto.com>
2024-11-25 06:43:54 +0000

    create the login & signup design

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,58 +18,68 @@ import {
 
 const Login = () => {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <>
+      <div className="w-full flex justify-center items-center h-[80vh]">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signup">Signup</TabsTrigger>
+            <TabsTrigger value="login">Login</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signup">
+            <Card>
+              <CardHeader>
+                <CardTitle>Signup</CardTitle>
+                <CardDescription>
+                  Create a new account and click signup when you're done.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Name</Label>
+                  <Input type="text" id="name" placeholder="Enter your Full Name" required />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="name">Email</Label>
+                  <Input type="email" id="email" placeholder="Enter your Email" required />
+                </div>
+
+                <div className="space-y-1">
+                  <Label htmlFor="name">Password</Label>
+                  <Input type="password" id="password" placeholder="Enter your Password" required />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Signup</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Login your password here. After signup, you'll be logged in.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="name">Email</Label>
+                  <Input type="email" id="email" placeholder="Enter your Email" required />
+                </div>
+
+                <div className="space-y-1">
+                  <Label htmlFor="name">Password</Label>
+                  <Input type="password" id="password" placeholder="Enter your Password" required />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Login</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   )
 }
-export default Logi;
+export default Login;
