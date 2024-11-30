06942128672078e69a3ff370a5a9b11337736f4c
9090856788 <kanhucharansahoo595@gmail.com>
2024-11-30 15:09:10 +0530

    make database connection asynchronous

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 
-const dbConnection = () => {
-  mongoose
-    .connect(
-      "mongodb+srv://kanhucharansahoo595:Kanhu143@lms.wzc0l.mongodb.net/?retryWrites=true&w=majority&appName=lms"
-    )
-    .then(() => {
-      console.log(`Database connected successfully`);
-    })
-    .catch((err) => {
-      console.error(`Error connecting to the database: ${err.message}`);
-    });
+const dbConnection = async () => {
+  try {
+    await mongoose
+      .connect(
+        "mongodb+srv://kanhucharansahoo595:Kanhu143@lms.wzc0l.mongodb.net/?retryWrites=true&w=majority&appName=lms"
+      )
+      .then(() => {
+        console.log(`Database connected successfully`);
+      })
+      .catch((err) => {
+        console.error(`Error connecting to the database: ${err.message}`);
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default dbConnection;
